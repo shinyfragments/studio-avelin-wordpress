f01d62a876166321119f7135c4f97f6d1cba1d60
Michael <michael.fiebus@gmail.com>
2026-06-30 21:43:03 +0200

    Finalize standalone homepage template

--- studio-avelin-child/front-page.php
+++ studio-avelin-child/front-page.php
@@ -3,10 +3,17 @@
  * Studio Avelin Front Page
  */
 
-get_header();
-
 $sa_portrait_url = esc_url(get_stylesheet_directory_uri() . '/assets/img/portrait.jpg');
 ?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class('sa-front-page'); ?>>
+<?php wp_body_open(); ?>
 
 <header class="sa-front-header" aria-label="Studio Avelin homepage navigation">
   <div class="sa-front-header__inner">
@@ -649,5 +656,6 @@ $sa_portrait_url = esc_url(get_stylesheet_directory_uri() . '/assets/img/portrai
   </div>
 </footer>
 
-<?php
-get_footer();
+<?php wp_footer(); ?>
+</body>
+</html>
